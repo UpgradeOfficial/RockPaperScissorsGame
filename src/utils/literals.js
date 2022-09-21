db8c16f2e6b60644b25ac12a2fb2b2b2274d5069
UpgradeOfficial <odeyemiincrease@yahoo.com>
2022-09-21 18:23:24 +0100

    changed the url for the backend host

--- src/utils/literals.js
+++ src/utils/literals.js
@@ -1,2 +1,2 @@
-export const allowedBackendHost = process.env.REACT_APP_ALLOWED_BACKEND_HOST || "https://rpcgame-api.onrender.com";
+export const allowedBackendHost = process.env.REACT_APP_ALLOWED_BACKEND_HOST || "https://rpcgame-api.cyclic.app/";
 export const node_env = process.env.NODE_DEV || process.env.REACT_APP_NODE_DEV || "local";
